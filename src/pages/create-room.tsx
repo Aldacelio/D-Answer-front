f164b4950a68646409705bc1ea2362172648337f
Aldacelio <aldacelio@alu.ufc.br>
2024-08-08 15:26:18 -0300

    feat: implement room creation with backend integration

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,44 +1,60 @@
-import { ArrowRight } from 'lucide-react'
+import { ArrowRight } from "lucide-react";
+import { toast } from "sonner";
+import { useNavigate } from "react-router-dom";
 
-import logo from '../assets/logo.png'
-import { useNavigate } from 'react-router-dom'
- 
+import logo from "../assets/logo.png";
+import { createRoom } from "../http/create-room";
 
 export function CreateRoom() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  async function handleCreateRoom(data: FormData) {
+    const theme = data.get("theme")?.toString();
 
-    function handleCreateRoom(data : FormData) {
-        const theme = data.get('theme')?.toString()
-        console.log(theme)
-
-        navigate('/room/new')
+    if (!theme) {
+      return;
     }
 
+    try {
+      const { roomId } = await createRoom({ theme });
 
-    return (
-        <main className="h-screen flex items-center justify-center px-4">
-            <div className="max-w-[450px] items-center flex flex-col gap-4">
-                <img src={logo} alt="logo" className='w-10'/>
-                <p className='leading-relaxed text-zinc-300 text-center'>
-                    crie uma sala pública na D-Answer e priorize as perguntas mais importantes para a comunidade.
-                </p>   
+      navigate(`/room/${roomId}`);
+    } catch {
+      toast.error("Houve um erro ao criar a sala. Tente novamente");
+    }
+  }
 
-                <form action={handleCreateRoom} className="w-full flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-0 ring-offset-zinc-900 focus-within:ring-1">
-                    <input 
-                        type="text"
-                        name="theme"
-                        placeholder="Nome da sala" 
-                        autoComplete="off"
-                        className="flex-1 text-sm bg-transparent mx-2 outline-none text-zinc-100 placeholder:text-zinc-500" 
-                    />
+  return (
+    <main className="h-screen flex items-center justify-center px-4">
+      <div className="max-w-[450px] items-center flex flex-col gap-4">
+        <img src={logo} alt="logo" className="w-10" />
+        <p className="leading-relaxed text-zinc-300 text-center">
+          crie uma sala pública na D-Answer e priorize as perguntas mais
+          importantes para a comunidade.
+        </p>
 
-                    <button type="submit" className="bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm hover:bg-orange-500 transition-colors">
-                        Criar sala
-                        <ArrowRight className="size-4" />
-                    </button>
-                </form>
-            </div>
-        </main>
-    )
+        <form
+          action={handleCreateRoom}
+          className="w-full flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-0 ring-offset-zinc-900 focus-within:ring-1"
+        >
+          <input
+            type="text"
+            name="theme"
+            placeholder="Nome da sala"
+            autoComplete="off"
+            className="flex-1 text-sm bg-transparent mx-2 outline-none text-zinc-100 placeholder:text-zinc-500"
+            required
+          />
+
+          <button
+            type="submit"
+            className="bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm hover:bg-orange-500 transition-colors"
+          >
+            Criar sala
+            <ArrowRight className="size-4" />
+          </button>
+        </form>
+      </div>
+    </main>
+  );
 }
