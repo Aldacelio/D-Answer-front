1f8b5054692732fba5d20787dea41e3e8a473878
Aldacelio <aldacelio@alu.ufc.br>
2024-08-08 12:49:33 -0300

    feat: Added design for create-room

---
+++ src/pages/create-room.tsx
@@ -0,0 +1,32 @@
+import { ArrowRight } from 'lucide-react'
+
+import logo from '../assets/logo.png';
+ 
+
+export function CreateRoom() {
+    return (
+        <main className="h-screen flex items-center justify-center px-4">
+            <div className="max-w-[450px] items-center flex flex-col gap-4">
+                <img src={logo} alt="logo" className='w-10'/>
+                <p className='leading-relaxed text-zinc-300 text-center'>
+                    crie uma sala pública na D-Answer e priorize as perguntas mais importantes para a comunidade.
+                </p>   
+
+                <form className="w-full flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-0 ring-offset-zinc-900 focus-within:ring-1">
+                    <input 
+                        type="text"
+                        name="theme"
+                        placeholder="Nome da sala" 
+                        autoComplete="off"
+                        className="flex-1 text-sm bg-transparent mx-2 outline-none text-zinc-100 placeholder:text-zinc-500" 
+                    />
+
+                    <button type="submit" className="bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm hover:bg-orange-500 transition-colors">
+                        Criar sala
+                        <ArrowRight className="size-4" />
+                    </button>
+                </form>
+            </div>
+        </main>
+    )
+}
